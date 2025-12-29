046baaa394da5917fda37fec033e9911590cc110
acandoo <117209328+acandoo@users.noreply.github.com>
2025-12-28 23:03:50 -0500

    fix(get_line/js): fix implementation to work for single call of get_line

--- src/externs_ffi.mjs
+++ src/externs_ffi.mjs
@@ -2,14 +2,17 @@ import { stdin as input, stdout as output } from 'node:process'
 import { createInterface } from 'node:readline'
 
 const rl = createInterface({ input, output })
-let cbDepth = 0
+const cbStack = [false, true]
 
 export function getLine(prompt, cb) {
-    const fnDepth = cbDepth
-    cbDepth++
+    const fnDepth = cbStack[0] ? 1 : 0
+    cbStack[fnDepth] = true
+    cbStack[1 - fnDepth] = false
+    console.log(cbStack)
     rl.question(prompt, (answer) => {
         cb(answer)
-        if (cbDepth === fnDepth) rl.close()
-        cbDepth--
+
+        // if the value wasn't flipped there is no nested getLine
+        if (cbStack[fnDepth]) rl.close()
     })
 }
